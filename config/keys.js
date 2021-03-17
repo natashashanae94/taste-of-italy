558ea923a9e5b51fbd28f75a4e5f174288ea0c3c
Natasha Johnson <natashashanae94@gmail.com>
2021-03-16 20:49:13 -0400

    made some changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: ,
+    mongoURI: "mongodb+srv://new_user2:FBQbVSCfzGRgRAax@cluster0.ucawd.mongodb.net/launchHouse?retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
